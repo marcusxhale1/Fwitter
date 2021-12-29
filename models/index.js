f318d283c794da20fd9330d9e02bc09afe902317
Marcus Hale <marcusxhale1@gmail.com>
2021-12-28 18:41:34 -0800

    post routes updated, and connected to index.js file

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
+const Post = require('./Post')
 
-module.exports = { User };
+// User can create many posts 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// Post was created by X = User 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+module.exports = { User, Post };
