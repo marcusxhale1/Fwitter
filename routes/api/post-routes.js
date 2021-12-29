18616acb48218b78f412595699e1894c8c79d20f
Marcus Hale <marcusxhale1@gmail.com>
2021-12-29 00:39:14 -0800

    you can delete a fweet now

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -58,6 +58,7 @@ router.get('/', (req, res) => {
       });
   });
 
+  //Updating someone's fweet
   router.put('/:id', (req, res) => {
     Post.update(
       {
@@ -81,5 +82,24 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-
+  
+  // deleteing a fweet
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
   module.exports = router; 
