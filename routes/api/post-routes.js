9c5281146023b3e6de023b38b7c5a56930120f91
Adam Menkes <amenkes24@gmail.com>
2021-12-30 17:15:19 -0800

    refractor code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -21,27 +21,13 @@ router.get("/", (req, res) => {
         attributes: ["username"],
       },
     ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // GET a SPECIFIC post
 router.get("/:id", (req, res) => {
@@ -63,76 +49,33 @@ router.get("/:id", (req, res) => {
         model: User,
         attributes: ["username"],
       },
-      attributes: [
-        'id', 
-        'fweet', 
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No fweets found with this user' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  router.post('/', (req, res) => {
-    // expects {Fweet: 'Just a random fweet', user_id: 1}
-    Post.create({
-      fweet: req.body.fweet,
-      user_id: req.body.user_id
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  // PUT /api/posts/Vote
-  router.put('/upvote', (req, res) =>{
-      Vote.create({
-          user_id: req.body.user_id,
-          post_id: req.body.post_id
-      })
-      .then(() => {
-        // then find the post we just voted on
-        return Post.findOne({
-          where: {
-            id: req.body.post_id
-          },
-          attributes: [
-            'id',
-            'fweet',
-            'created_at',
-            // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `like_count`
-            [
-                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                'vote_count'
-            ]
-          ]
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        })
-
   })
-  });
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// CREATE a Post
+router.post("/", (req, res) => {
+  Post.create({
+    fweet: req.body.fweet,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // CREATE a Like by UPDATING a post
 router.put('/like', (req, res) => {
@@ -155,27 +98,23 @@ router.put("/:id", (req, res) => {
       where: {
         id: req.params.id,
       },
-      {
-        where: {
-          id: req.params.id
-        }
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
       }
-    )
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No fweet found with this user' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
-  // deleteing a fweet
-  router.delete('/:id', (req, res) => {
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a Post
+router.delete('/:id', (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
@@ -193,4 +132,6 @@ router.put("/:id", (req, res) => {
         res.status(500).json(err);
       });
   });
-  module.exports = router; 
+
+
+module.exports = router;
