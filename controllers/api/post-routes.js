9f1c11e69937a16fa572cc9e1238d95607b4cc16
Ashley Cho <ashleycho.slp@gmail.com>
2022-01-02 09:18:24 -0500

    front end

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -8,14 +8,14 @@ router.get("/", (req, res) => {
     order: [['created_at', 'DESC']],
     attributes: ["id", "fweet", "created_at", [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'like_count']],
     include: [
-        {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ["username"],
@@ -37,14 +37,14 @@ router.get("/:id", (req, res) => {
     },
     attributes: ["id", "fweet", "created_at", [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'like_count']],
     include: [
-        {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ["username"],
@@ -68,7 +68,7 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   Post.create({
     fweet: req.body.fweet,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -79,14 +79,18 @@ router.post("/", (req, res) => {
 
 // CREATE a Like by UPDATING a post
 router.put('/like', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.like(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
+  // custom static method created in models/Post.js
+  const request = {
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  }
+  Post.like(request, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.error("Upvote Error:", err);
+      res.status(400).json(err);
+    });
+});
 
 // UPDATE a Post
 router.put("/:id", (req, res) => {
@@ -115,23 +119,23 @@ router.put("/:id", (req, res) => {
 
 // DELETE a Post
 router.delete('/:id', (req, res) => {
-    Post.destroy({
-      where: {
-        id: req.params.id
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
       }
+      res.json(dbPostData);
     })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path')
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -34,5 +35,5 @@ app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on PORT', PORT));
 });
