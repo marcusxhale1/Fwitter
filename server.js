01c620fd7b898bc7b99adeefb9f48f5e6a00838a
Adam Menkes <amenkes24@gmail.com>
2022-01-04 10:18:11 -0800

    code for dashboard routes and frontend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const sess = {
   };
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
